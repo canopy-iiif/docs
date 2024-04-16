238256b9dea09023e62c7e556dbcb2635a7ac3ae
Mat Jordan <mat@northwestern.edu>
2024-04-16 00:01:27 -0400

    Add sitemap.

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://canopy-iiif.github.io/docs",
+  changefreq: "weekly",
+  priority: 0.7,
+  generateRobotsTxt: true,
+  sitemapSize: 100,
+  outDir: "./out",
+};
